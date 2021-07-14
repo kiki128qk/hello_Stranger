ebc7b94ea01ee8076beafa17f62c5c9e9b84e01f
jung_jung <kiki128qk@naver.com>
2021-07-14 23:02:18 +0900

    네입게이션 작업/ 배경 mp4 백업x

--- index.php
+++ index.php
@@ -4,9 +4,30 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <meta name="keywords" content="포트폴리오, 웹퍼블리셔, 신입, 색다른">
+  <meta name="description" content="신입 웹퍼블리셔의 메인 포트폴리오 사이트">
+  <title>Hello Stranger</title>
+  <!-- css link -->
+  <link rel="stylesheet" href="/Hello_Stranger/css/reset.css">
+  <link rel="stylesheet" href="/Hello_Stranger/css/style.css">
+  <link rel="stylesheet" href="/Hello_Stranger/css/modia.css">
+  
+
 </head>
 <body>
+  <div class="wrap">
+    <header>
+      <nav>
+          <ul>
+              <li>Home</li>
+              <li>Work</li>
+              <li>Pofile</li>
+              <li>Contact</li> 
+          </ul>
+      </nav>
+      <video src="/Hello_Stranger/data/background.mp4">
+    </header>
+  </div>
   
 </body>
 </html>
